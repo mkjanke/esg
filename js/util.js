3a29b76c9f7556dcc30fd4d571b8aac5734ec1f9
Michael Janke <mkjanke@gmail.com>
2018-01-15 16:40:48 -0600

    Basi NFR rendering, Some UI tweaks.

--- js/util.js
+++ js/util.js
@@ -1,9 +1,14 @@
-var jsonObjMetrics = {}; //yes, I know thses are global
-var metrics_json_file = "../req/req-metric.json";
+export var jsonObjMetrics = {}; //yes, I know thses are global
+export var metrics_json_file = "../req/req-metric.json";
 
-function LoadMetricsFromJSON() {
-    //jQuery - get JSON from server
-    //datatables.net library - load and populate Metric table
+export var jsonObjNFR = {}; 
+export var nfr_json_file = "../nfr/nfr-body.json";
+
+//These functions Use
+//jQuery - get JSON from server
+//datatables.net library - load and populate Metric table
+
+export function LoadMetricsFromJSON() {
     $.get(metrics_json_file, function (data) {
         jsonObjMetrics = data.Metrics;
         $('#MetricTable').DataTable({
@@ -19,6 +24,27 @@ function LoadMetricsFromJSON() {
                     "data": "REPLACES",
                     "defaultContent": ""
                 }
+            ]
+        });
+    }, 'json');
+    //Nuke the 'wait' messsage
+    $('#wait').text("");
+}
+
+export function LoadNFRSFromJSON() {
+    $.get(nfr_json_file, function (data) {
+        jsonObjNFR = data.Metrics;
+        $('#NFRTable').DataTable({
+            "data": jsonObjNFR,
+            "columns": [
+                { "data": "Index" },
+                { "data": "Category" },
+                { "data": "Context"},
+                { "data": "Goals" },
+                { "data": "Rationale" },
+                { "data": "Requirement" },
+                { "data": "Scale" },
+                { "data": "Status" }
             ]
         });
     }, 'json');
